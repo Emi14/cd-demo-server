519b153d2e571dfc744ddf41b93132db64352503
George <georgeserbanescu@hotmail.com>
2017-05-01 22:30:45 +0300

    remove listen

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-app.listen(3000, function () {
-  console.log('app listening on port 3000!')
-})
+// app.listen(3000, function () { 
+//   console.log('app listening on port 3000!')
+// })
 
 
 // module.exports = app;
